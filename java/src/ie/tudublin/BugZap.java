b4269eb300e3fadfb3763efc3cfcdab747400ee7
Sarah Barron <sarahbarron6@gmail.com>
2023-02-01 16:10:25 +0000

    Saved

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -18,10 +18,15 @@ public class BugZap extends PApplet
 		playerY = height-25;
 		playerWidth = 25;
 
+		bugX = width/2;
+		bugY = 15;
+		bugHeight = 10;
+
 		smooth();
 		
 	}
 	float playerX, playerY, playerWidth;
+	float bugX, bugY, bugHeight;
 
 	public void drawPlayer(float x, float y, float w)
 	{
@@ -30,12 +35,21 @@ public class BugZap extends PApplet
 		float h = w/2;
 		rect(x,y,w,h);
 		
+	}
+	public void drawBug(float x, float y, float h)
+	{
+		strokeWeight(2);
+		stroke(255);
+		triangle(x, y, x-(h/2), y-h, x+(h/2),y-h);
+		
+		
 	}	
 	
 	public void draw()
 	{	
-
+		background(0);
 		drawPlayer(playerX, playerY, playerWidth);
+		drawBug(bugX, bugY, bugHeight);
 
 	}
 
@@ -44,14 +58,31 @@ public class BugZap extends PApplet
 		if (keyCode == LEFT)
 		{
 			System.out.println("Left arrow pressed");
+			if(playerX > 0)
+			{
+				playerX--;
+			}
+			else
+			{
+				playerX += 10;
+			}
 		}
 		if (keyCode == RIGHT)
 		{
 			System.out.println("Right arrow pressed");
+			if(playerX < width)
+			{
+				playerX++;
+			}
+			else
+			{
+				playerX -= 10;
+			}
 		}
 		if (key == ' ')
 		{
 			System.out.println("SPACE key pressed");
+			line(playerX+playerWidth/2,playerY, playerX+playerWidth/2, 0);
 		}
 	}
 		
