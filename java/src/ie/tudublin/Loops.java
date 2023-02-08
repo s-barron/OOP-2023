3b5264410ec2d4a1c1ece67c8dc3ecd795e8b5e6
Sarah Barron <sarahbarron6@gmail.com>
2023-02-08 17:37:58 +0000

    For Loops: (i)

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -5,6 +5,7 @@ import processing.core.PApplet;
 public class Loops extends PApplet {
 
 	int mode = 0;
+	int color = 0;
 	public void settings() {
 		size(1000, 1000);
 	}
@@ -22,59 +23,14 @@ public class Loops extends PApplet {
 	}
 
 	public void draw() {
+		noStroke();
 		background(0);
 		//fill(255);
-		fill(180,100,100);
-		switch(mode)
+		for(int i = 0; i < 10; i++)
 		{
-			case 0:
-			{
-				if(mouseX > width/4 && mouseX < width/2)
-				{					
-					rect(width/4,0, width/4, height);
-				}
-				else if(mouseX > width/2 && mouseX < width*0.75)
-				{
-					rect(width/2,0, width/4, height);
-				}
-				break;
-
-			}
-			case 1:
-			{
-				if(mouseX > width/4 && mouseX < width/2 && mouseY < height/2)
-				{					
-					rect(width/4, 0, width/4, height/2);
-				}
-				else if(mouseX > width/4 && mouseX < width/2 && mouseY > height/2)
-				{					
-					rect(width/4, height/2 , width/4, height/2);
-				}
-				else if(mouseX > width/2 && mouseX < width*0.75 && mouseY < height/2)
-				{
-					rect(width/2, 0, width/4, height/2);
-				}
-				else if(mouseX > width/2 && mouseX < width*0.75 && mouseY > height/2)
-				{
-					rect(width/2, height/2, width/4, height/2);
-				}	
-				break;				
-			}
-			case 2:
-			{
-				rect(375, 400, width/4, height/8);
-				if(mouseX > 375 && mouseX < 375+width/4 && mouseY > 400 && mouseY < 400+height/8)
-				{
-					fill(0,100,100);
-					rect(375, 400, width/4, height/8);
-				}
-			}
-
-
+			fill(color + (i*25),100,100);
+			rect(i*(width/10), 0, width/10, height);
 		}
-		
-		
-		
-
 	}
+
 }
