f17bae1e882b108a2483e9d8b4a50ae51915d10f
Sarah Barron <sarahbarron6@gmail.com>
2023-02-13 11:56:30 +0000

    StarMap V1

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -10,16 +10,39 @@ public class StarMap extends PApplet
 	}
 
 	public void setup() {
-		colorMode(HSB);
-		background(0);
+		colorMode(HSB,360,100,100);
+		background(0,0,0);
 
 		smooth();
 		
 	}
 
+	int x,y,w,h;
+	public void drawGrid()
+	{
 		
+		x = 0;
+		y = 0;
+		w = width/11;
+		h = height/11;
+
+		for(int i = 0; i < 11; i++)
+		{
+			
+			for(int j = 0; j < 11; j++)
+			{
+				rect(x,y,w,h);
+				x += w;
+			}
+			x = 0;
+			y += h;	
+		}
+
+	}
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);
+		stroke(90,100,100);
+		drawGrid();	
 	}
 }
