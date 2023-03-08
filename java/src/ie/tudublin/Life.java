b270626bc289eba09831b21705500013c2f21be2
Sarah Barron <sarahbarron6@gmail.com>
2023-03-08 16:58:20 +0000

    Added randomise and clear

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -18,6 +18,34 @@ public class Life extends PApplet
 		board.randomise();
 	}
 
+	int mode = 0;
+
+	public void keyPressed() {
+		if (key >= '0' && key <= '9') {
+			mode = key - '0';
+		}
+		switch(mode)
+		{
+			case 1:
+			{
+				board.randomise();	
+				break;
+			}
+			case 2:
+			{
+				for(int row = 0; row < 50; row++)
+				{
+					for(int col = 0; col < 50; col++)
+					{
+						board.board[row][col] = false;
+					}
+				}
+				break;
+			}
+		}
+			
+	}
+
 	public void draw()
 	{	
 		background(0);
